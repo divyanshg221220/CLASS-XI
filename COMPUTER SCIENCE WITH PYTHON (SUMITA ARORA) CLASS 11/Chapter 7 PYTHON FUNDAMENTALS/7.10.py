52cc552239cfce3603cf1c3eb25456aa058320e2
divyanshg221220 <divyanshg221220@outlook.com>
2026-06-19 13:18:40 +0530

    AREA

---
+++ COMPUTER SCIENCE WITH PYTHON (SUMITA ARORA) CLASS 11/Chapter 7 PYTHON FUNDAMENTALS/7.10.py
@@ -0,0 +1,4 @@
+#7.10
+b=float(input("Enter base of a triangle:"))
+h=float(input("Enter height of a triangle:"))
+print("Area of a triangle:",0.5*b*h)
--- COMPUTER SCIENCE WITH PYTHON (SUMITA ARORA) CLASS 11/Chapter 7 PYTHON FUNDAMENTALS/7.6.py
+++ COMPUTER SCIENCE WITH PYTHON (SUMITA ARORA) CLASS 11/Chapter 7 PYTHON FUNDAMENTALS/7.6.py
@@ -1,3 +1,3 @@
 #7.6
 r=float(input("Enter the radius of a circle:"))
-print("Area:",3.14*r*r)
+print("Area of a circle:",3.14*r*r)
