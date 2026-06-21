d7872d17d3d395054ac91414d44ae0110383104d
divyanshg221220 <divyanshg221220@outlook.com>
2026-06-21 18:24:28 +0530

    fix

--- COMPUTER SCIENCE WITH PYTHON (SUMITA ARORA) CLASS 11/Chapter 7 PYTHON FUNDAMENTALS/7.7.py
+++ COMPUTER SCIENCE WITH PYTHON (SUMITA ARORA) CLASS 11/Chapter 7 PYTHON FUNDAMENTALS/7.7.py
@@ -1,5 +1,5 @@
 #7.7
 l=[]
 for i in range(5):
-    l.append(float(input("Enter marks of subject",i+1,":")))
+    l.append(float(input("Enter marks of subject "+str(i+1)+":")))
 print("Average:",sum(l)/len(l))
