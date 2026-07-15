36cbc755e1fd2e67cba7450f6ce9841e953535dc
divyanshg221220 <divyanshg221220@outlook.com>
2026-07-15 22:39:15 +0530

    Add program to calculate A to the power of B based on user input

---
+++ COMPUTER SCIENCE WITH PYTHON (SUMITA ARORA) CLASS 11/Chapter 8 DATA HANDLING/8.12.py
@@ -0,0 +1,5 @@
+#8.12
+a=int(input("Random number between 0 and 5 (A) : "))
+b=int(input("Random number between 0 and 5 (B)  : "))
+if a>=0 and a<=5 and b>=0 and b<=5:
+    print("A to the power B =",a**b)
