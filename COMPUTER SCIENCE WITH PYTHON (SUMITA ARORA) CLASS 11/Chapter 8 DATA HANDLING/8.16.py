1c5f5d21b899fa49e6612729d1b439351e569e3a
divyanshg221220 <divyanshg221220@outlook.com>
2026-07-16 23:04:20 +0530

    Add new program to calculate mean, median, and mode of six random numbers

---
+++ COMPUTER SCIENCE WITH PYTHON (SUMITA ARORA) CLASS 11/Chapter 8 DATA HANDLING/8.16.py
@@ -0,0 +1,23 @@
+#8.16
+import random
+s=0
+l=[]
+for i in range(6):
+    r=random.random()
+    print(r)
+    s+=r
+    l.append(r)
+print("mean:",s/6)
+l=sorted(l)
+mid=len(l)//2
+print("median:",(l[mid-1]+l[mid])/2)
+max_count=0
+mode=0
+for i in l:
+    count=l.count(i)
+    if count>max_count:
+        max_count=count
+        mode=i
+    elif count==max_count:
+        mode=i
+print("mode:",mode)
