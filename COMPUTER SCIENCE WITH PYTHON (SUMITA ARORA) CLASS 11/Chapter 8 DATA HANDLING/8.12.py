f2f3a50e43e8159834a1f16ad5f7d635ddfc8889
divyanshg221220 <divyanshg221220@outlook.com>
2026-07-16 22:53:15 +0530

    Fix random number generation in 8.12.py to use randint instead of randrange

--- COMPUTER SCIENCE WITH PYTHON (SUMITA ARORA) CLASS 11/Chapter 8 DATA HANDLING/8.12.py
+++ COMPUTER SCIENCE WITH PYTHON (SUMITA ARORA) CLASS 11/Chapter 8 DATA HANDLING/8.12.py
@@ -1,7 +1,7 @@
 #8.12
 import random
-a=random.randrange(0,5)
-b=random.randrange(0,5)
+a=random.randint(0,5)
+b=random.randint(0,5)
 print("Random number between 0 and 5 (A) :",a)
 print("Random number between 0 and 5 (B) :",b)
 print("A to the power B =",a**b)
