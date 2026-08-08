8333b229bee5d4884d62b5ba4c0173029d301043
divyanshg221220 <divyanshg221220@outlook.com>
2026-08-08 20:45:29 +0530

    Refine output messages for prime number check in program

--- COMPUTER SCIENCE WITH PYTHON (SUMITA ARORA) CLASS 11/Chapter 9 FLOW OF CONTROL/9.8.py
+++ COMPUTER SCIENCE WITH PYTHON (SUMITA ARORA) CLASS 11/Chapter 9 FLOW OF CONTROL/9.8.py
@@ -4,13 +4,13 @@ n=int(input("Enter a number:"))
 sqrt=math.sqrt(n)
 if sqrt==int(sqrt):
     if sqrt<2:
-        print("Not prime")
+        print(sqrt,"is not a prime number")
     else:
         for i in range(2,int(math.sqrt(sqrt))+1):
             if sqrt%i==0:
-                print("Not prime")
+                print(sqrt,"is not a prime number")
                 break
         else:
-            print("Prime")
+            print(sqrt,"is a prime number")
 else:
-    print("Not prime")
+    print(sqrt,"is not a prime number")
