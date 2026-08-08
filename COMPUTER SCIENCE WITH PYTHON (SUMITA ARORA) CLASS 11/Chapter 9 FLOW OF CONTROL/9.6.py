8cb5552834cd87ea8aebee626e9b47a2dc36bc72
divyanshg221220 <divyanshg221220@outlook.com>
2026-08-08 19:11:28 +0530

    Add program to validate if three sides can form a triangle

--- COMPUTER SCIENCE WITH PYTHON (SUMITA ARORA) CLASS 11/Chapter 9 FLOW OF CONTROL/9.6.py
+++ COMPUTER SCIENCE WITH PYTHON (SUMITA ARORA) CLASS 11/Chapter 9 FLOW OF CONTROL/9.6.py
@@ -1,1 +1,8 @@
 #9.6
+side1=float(input("Enter side 1:"))
+side2=float(input("Enter side 2:"))
+side3=float(input("Enter side 3:"))
+if (side1+side2>side3) and (side2+side3>side1) and (side3+side1>side2):
+    print("Valid triangle")
+else:
+    print("Invalid triangle")
