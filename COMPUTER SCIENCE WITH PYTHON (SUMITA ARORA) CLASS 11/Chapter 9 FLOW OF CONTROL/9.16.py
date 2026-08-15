8a62c8f920c4e8d0f2e13827468b825a83f02dca
divyanshg221220 <divyanshg221220@outlook.com>
2026-08-15 13:15:50 +0530

    Fix input prompts for consistency in number entry

--- COMPUTER SCIENCE WITH PYTHON (SUMITA ARORA) CLASS 11/Chapter 9 FLOW OF CONTROL/9.16.py
+++ COMPUTER SCIENCE WITH PYTHON (SUMITA ARORA) CLASS 11/Chapter 9 FLOW OF CONTROL/9.16.py
@@ -1,7 +1,7 @@
 #9.16
-N=int(input("Enter the number:"))
+N=int(input("Enter a number:"))
 l=[]
 for i in range(N):
-    l.append(int(input("Enter the number:")))
+    l.append(int(input("Enter a number:")))
 l.sort()
 print("Second largest number:",l[-2])
