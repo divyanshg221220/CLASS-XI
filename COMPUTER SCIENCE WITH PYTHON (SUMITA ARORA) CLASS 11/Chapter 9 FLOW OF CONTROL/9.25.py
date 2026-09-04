d6bdd1906fe482d49244924c686e267a4c72906e
divyanshg221220 <divyanshg221220@outlook.com>
2026-09-04 18:06:47 +0530

    Add program to display various patterns based on user choice

---
+++ COMPUTER SCIENCE WITH PYTHON (SUMITA ARORA) CLASS 11/Chapter 9 FLOW OF CONTROL/9.25.py
@@ -0,0 +1,55 @@
+#9.25
+#9.24
+while True:
+    print("""1.
+A
+A B
+A B C
+A B C D
+A B C D E
+A B C D E F
+""")
+    print("""2.
+A
+B B
+C C C
+D D D D
+E E E E E""")
+    print("""3.
+0
+2 2
+4 4 4
+6 6 6 6
+8 8 8 8 8""")
+    print("""4.
+2
+4 4
+6 6 6
+8 8 8 8""")
+    print("5. EXIT")
+    print()
+    choice=int(input("USER'S CHOICE:"))
+    if choice==1:
+        for i in range(1,7):
+            for j in range(65,65+i):
+                print(chr(j),end=" ")
+            print()
+    elif choice==2:
+        for i in range(0,5):
+            for j in range(0,i+1):
+                print(chr(65+i),end=" ")
+            print()
+    elif choice==3:
+        for i in range(0,5):
+            for j in range(0,i+1):
+                print(i*2,end=" ")
+            print()
+    elif choice==4:
+        for i in range(1,5):
+            for j in range(0,i):
+                print(i*2,end=" ")
+            print()
+    elif choice==5:
+        print("EXITED BY USER")
+        break
+    print()
