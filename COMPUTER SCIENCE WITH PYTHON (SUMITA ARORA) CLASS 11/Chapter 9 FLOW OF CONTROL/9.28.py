c3132884c7ea757894b46888fa7179d11fda5264
divyanshg221220 <divyanshg221220@outlook.com>
2026-09-04 18:51:44 +0530

    Normalize case in temperature output messages

--- COMPUTER SCIENCE WITH PYTHON (SUMITA ARORA) CLASS 11/Chapter 9 FLOW OF CONTROL/9.28.py
+++ COMPUTER SCIENCE WITH PYTHON (SUMITA ARORA) CLASS 11/Chapter 9 FLOW OF CONTROL/9.28.py
@@ -3,7 +3,7 @@ t=float(input("Enter temperature:"))
 u=input("Enter unit (C/F):")
 if u=='C':
     f=(t*9/5)+32
-    print("Temperature in Fahrenheit:",f)
+    print("Temperature in fahrenheit:",f)
 elif u=='F':
     c=(t-32)*5/9
-    print("Temperature in Celsius:",c)
+    print("Temperature in celsius:",c)
