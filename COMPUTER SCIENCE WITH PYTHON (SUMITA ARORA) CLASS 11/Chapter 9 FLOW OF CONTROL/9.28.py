f12988fa9f66c5748eb6c43e5fed6f87dd801d62
divyanshg221220 <divyanshg221220@outlook.com>
2026-09-04 18:49:55 +0530

    Add temperature conversion program between Celsius and Fahrenheit

---
+++ COMPUTER SCIENCE WITH PYTHON (SUMITA ARORA) CLASS 11/Chapter 9 FLOW OF CONTROL/9.28.py
@@ -0,0 +1,9 @@
+#9.28
+t=float(input("Enter temperature:"))
+u=input("Enter unit (C/F):")
+if u=='C':
+    f=(t*9/5)+32
+    print("Temperature in Fahrenheit:",f)
+elif u=='F':
+    c=(t-32)*5/9
+    print("Temperature in Celsius:",c)
