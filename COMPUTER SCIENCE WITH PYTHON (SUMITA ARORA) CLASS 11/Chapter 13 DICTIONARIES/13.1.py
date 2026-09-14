6318142f57ff1e56e453524be478dcf60e194b3e
divyanshg221220 <divyanshg221220@outlook.com>
2026-09-14 21:24:47 +0530

    Add program to input employee names and salaries into a dictionary

--- COMPUTER SCIENCE WITH PYTHON (SUMITA ARORA) CLASS 11/Chapter 13 DICTIONARIES/13.1.py
+++ COMPUTER SCIENCE WITH PYTHON (SUMITA ARORA) CLASS 11/Chapter 13 DICTIONARIES/13.1.py
@@ -1,1 +1,8 @@
 #13.1
+n=int(input("Enter number of employees:"))
+d={}
+for i in range(n):
+    name=input("Enter name of employee:")
+    salary=int(input("Enter salary of employee:"))
+    d[name]=salary
+print(d)
