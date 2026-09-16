1004c0513d5e014537007f40810db7b9db20e376
DIVYANSH GUPTA <divyanshg221220@outlook.com>
2026-09-16 09:44:16 +0000

    Add program to sort a dictionary by its values in descending order

--- COMPUTER SCIENCE WITH PYTHON (SUMITA ARORA) CLASS 11/Chapter 14 UNDERSTANDING SORTING/14.1.py
+++ COMPUTER SCIENCE WITH PYTHON (SUMITA ARORA) CLASS 11/Chapter 14 UNDERSTANDING SORTING/14.1.py
@@ -1,1 +1,10 @@
 #14.1
+Election=eval(input("Enter a dictionary:"))
+ListA=[]
+ListB=[]
+for i in Election:
+    ListA.append(i)
+    ListB.append(Election[i])
+l=sorted(Election,key=Election.get,reverse=True)
+for i in l:
+    print(ListA[ListB.index(Election[i])])
