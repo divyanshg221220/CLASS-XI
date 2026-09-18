094d1f1bf70635394640de8005239726eafe4e8d
DIVYANSH GUPTA <divyanshg221220@outlook.com>
2026-09-18 10:16:54 +0000

    .

---
+++ COMPUTER SCIENCE WITH PYTHON (SUMITA ARORA) CLASS 11/Chapter 14 UNDERSTANDING SORTING/14.2.py
@@ -0,0 +1,10 @@
+#14.2
+L=[("Wilhelm Conrad Röntgen","Physics",1901),("Ronald Ross","Medicine",1902),("Marie Curie","Physics",1903),("Ivan Pavlov","Medicine",1904),("Henryk Sienkiewicz","Literature",1905),("Theodore Roosevelt","Peace",1906)]
+for i in range(1,len(L)):
+    key=L[i]
+    lastname=key[0].split()[-1]
+    j=i-1
+    while j>=0 and L[j][0].split()[-1]>key:
+        L[j+1]=L[j]
+        j-=1
+    L[j+1]=key
